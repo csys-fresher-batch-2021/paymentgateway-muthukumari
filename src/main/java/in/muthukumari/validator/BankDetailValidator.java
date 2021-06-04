1280d26fb5e804d125cdfd8f6828a2666a620da9
MuthuKumari-P <muthukumari1999p@gmail.com>
2021-06-04 10:09:40 +0530

    adding extra validation

--- src/main/java/in/muthukumari/validator/BankDetailValidator.java
+++ src/main/java/in/muthukumari/validator/BankDetailValidator.java
@@ -1,7 +1,11 @@
 package in.muthukumari.validator;
 
+import java.sql.SQLException;
 import java.util.Map;
+import java.util.Set;
+
 import in.muthukumari.constants.AccountNumberLength;
+import in.muthukumari.dao.BankDAO;
 import in.muthukumari.exception.DBException;
 import in.muthukumari.model.CustomerBankDetail;
 
@@ -10,8 +14,6 @@ public class BankDetailValidator {
 	private BankDetailValidator() {
 		// Default constructor
 	}
-
-	
 
 	/**
 	 * This method validate the account number
@@ -35,6 +37,56 @@ public class BankDetailValidator {
 			throw new DBException("Bank Name Not Exists");
 		}
 		return isValidAccountNumber;
+	}
+	
+	/**
+	 * This method used to check the bank name
+	 * @param customer
+	 * @return
+	 * @throws ClassNotFoundException
+	 * @throws SQLException
+	 * @throws DBException
+	 */
+	public static boolean isValidBank(CustomerBankDetail customer) throws ClassNotFoundException, SQLException, DBException {
+		Set<String> bankNameList=BankDAO.getBankNameList();
+		boolean isValid=false;
+		if(bankNameList.contains(customer.getBankName())) {
+			isValid=true;
+		}
+		return isValid;
+	}
+
+	/**
+	 * This method used to check the atm card number
+	 * @param customer
+	 * @return
+	 */
+	public static boolean isValidAtmCardNumber(CustomerBankDetail customer) {
+		boolean isValidNum=false;
+		String atmNumStr=Long.toString(customer.getAtmNumber());
+		String regex=".*[0-9].*";
+		if(atmNumStr.length()==16 && atmNumStr.matches(regex)) {
+			isValidNum=true;
+		}
+		return isValidNum;
+	}
+
+	public static boolean isValidAtmPinNumber(CustomerBankDetail customer) {
+		boolean isValidNum=false;
+		String atmPinNumStr=Long.toString(customer.getAtmPinNumber());
+		if(atmPinNumStr.length()==4) {
+			isValidNum=true;
+		}
+		return isValidNum;
+	}
+
+	public static boolean isValidMobileNumber(CustomerBankDetail customer) {
+		boolean isValidNum=false;
+		String atmPinNumStr=Long.toString(customer.getAtmPinNumber());
+		if(atmPinNumStr.length()==10 && atmPinNumStr.charAt(0)=='0' ) {
+			isValidNum=true;
+		}
+		return isValidNum;
 	}
 
 }
--- src/test/java/in/muthukumari/CustomerBankDetailTest.java
+++ src/test/java/in/muthukumari/CustomerBankDetailTest.java
@@ -1,6 +1,9 @@
 package in.muthukumari;
 
 import static org.junit.Assert.*;
+
+import java.sql.SQLException;
+
 import org.junit.Test;
 
 import in.muthukumari.exception.DBException;
@@ -28,7 +31,7 @@ public class CustomerBankDetailTest {
 	public void testCase2() {
 		customer.setUserName("$56*&9");
 		boolean isValidName = CustomerDetailValidator.isValidName(customer.getUserName());
-		assertTrue(isValidName);
+		assertFalse(isValidName);
 	}
 	/**
 	 * Valid Account Number
@@ -49,8 +52,84 @@ public class CustomerBankDetailTest {
 	public void testCase4() throws DBException {
 		customer.setBankName("Indian Bank");
 		customer.setAccountNumber(99876457876787L);
-		boolean isValidBank=BankDetailValidator.isValidAccountNumber(customer);
-		assertFalse(isValidBank);
+		boolean isValidAccountNum=BankDetailValidator.isValidAccountNumber(customer);
+		assertFalse(isValidAccountNum);
 	}
+	/**
+	 * Valid bank name
+	 * @throws DBException 
+	 * @throws SQLException 
+	 * @throws ClassNotFoundException 
+	 */
+	@Test
+	public void testcase5() throws ClassNotFoundException, SQLException, DBException {
+		customer.setBankName("Indian Bank");
+		boolean isValidBank=BankDetailValidator.isValidBank(customer);
+		assertTrue(isValidBank);		
+	}
+	
+	/**
+	 * Invalid bank name
+	 * @throws DBException 
+	 * @throws SQLException 
+	 * @throws ClassNotFoundException 
+	 */
+	@Test
+	public void testcase6() throws ClassNotFoundException, SQLException, DBException {
+		customer.setBankName("United Bank");
+		boolean isValidBank=BankDetailValidator.isValidBank(customer);
+		assertFalse(isValidBank);		
+	}
+	
+	/**
+	 * Valid ATM card number
+	 */
+	@Test
+	public void testcase7() {
+		customer.setAtmNumber(8765365767898767L);
+		boolean isValidAtmNum=BankDetailValidator.isValidAtmCardNumber(customer);
+		assertTrue(isValidAtmNum);
+	}
+	
+	/**
+	 * Invalid ATM card number
+	 */
+	@Test
+	public void testcase8() {
+		customer.setAtmNumber(87653667898767L);
+		boolean isValidAtmNum=BankDetailValidator.isValidAtmCardNumber(customer);
+		assertFalse(isValidAtmNum);
+	}
+	
+	/**
+	 * Valid ATM pin number
+	 */
+	@Test
+	public void testcase9() {
+		customer.setAtmPinNumber(1234);
+		boolean isValidPinNumber=BankDetailValidator.isValidAtmPinNumber(customer);
+		assertTrue(isValidPinNumber);
+	}
+	
+	/**
+	 * Invalid ATM pin number
+	 */
+	@Test
+	public void testcase10() {
+		customer.setAtmPinNumber(12934);
+		boolean isValidPinNumber=BankDetailValidator.isValidAtmPinNumber(customer);
+		assertFalse(isValidPinNumber);
+	}
+	
+	/**
+	 * Invalid ATM pin number
+	 */
+	@Test
+	public void testcase11() {
+		customer.setAtmPinNumber(12934);
+		boolean isValidPinNumber=BankDetailValidator.isValidMobileNumber(customer);
+		assertFalse(isValidPinNumber);
+	}
+
 	
 }
